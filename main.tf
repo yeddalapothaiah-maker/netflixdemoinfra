1f89e7e07ef4bb27071b3318415ca3ff4b659e61
yeddalapothaiah-maker <yeddalapothaiah@gmail.com>
2026-08-24 17:57:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "one" {
 }
 
 variable "instance_names" {
-  default = ["jenkins", "tomcat-1", "tomcat-2", ]
+  default = ["jenkins", "tomcat-1", "tomcat-2"]
 }
 
 resource "aws_s3_bucket" "one" {
