6e6a5ad19a2a9098a817765f7bc5a0bf1f97f3c1
yeddalapothaiah-maker <yeddalapothaiah@gmail.com>
2026-08-24 17:37:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
   count                  = 3
-  ami                    = "ami-0b6d9d3d33ba97d99"
+  ami                    = "ami-0ac7b260cf76d8865"
   instance_type          = "c7i-flex.large"
-  key_name               = "junks"
-  vpc_security_group_ids = ["sg-0a1fb8aa0587d50af"]
+  key_name               = "pothaiahdocker"
+  vpc_security_group_ids = ["sg-052610925a2a09c3d"]
   tags = {
     Name = var.instance_names[count.index]
   }
 }
 
 variable "instance_names" {
-  default = ["jenkins", "tomcat-1", "Monitoring server"]
+  default = ["jenkins", "tomcat-1", "tomcat-2", ]
 }
 
 resource "aws_s3_bucket" "one" {
-  bucket = "my-project-bucket-123456789"
+  bucket = "pothaiah"
   versioning {
     enabled = true
   }
